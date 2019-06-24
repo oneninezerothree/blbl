4caff0f027d83be54e25262e84592d9fc3190840
chaMi <1870859365@qq.com>
2019-06-24 14:56:07 +0800

    :boom: 公共默认样式添加

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-
+import Login from './routes/Login/Login'
+import Register from './routes/Register/Register'
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
       </Switch>
     </Router>
   );
---
+++ src/routes/Register/Register.js
@@ -0,0 +1,18 @@
+import React ,{Component} from 'react'
+
+export default  class Register extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          注册
+        </header>
+      </div>
+    );
+  }
+
+}
