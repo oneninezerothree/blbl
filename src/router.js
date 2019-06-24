a0bc0fff7cab7d02ff69857f127fa3be4b888a40
chaMi <1870859365@qq.com>
2019-06-24 20:54:39 +0800

    :fire:登录注册以及个人页面完完成

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import Login from './routes/Login/Login'
 import Register from './routes/Register/Register'
+import My from './routes/My/My'
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
@@ -10,6 +11,7 @@ function RouterConfig({ history }) {
         <Route path="/" exact component={IndexPage} />
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
+        <Route path="/my" exact component={My} />
       </Switch>
     </Router>
   );
